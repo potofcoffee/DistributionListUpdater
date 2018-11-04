4d9209838c53091dbc5c62c7b840a98b2929614e
Christoph Fischer <Christoph.Fischer@elkw.de>
2018-11-04 16:31:46 +0100

    Projektdateien hinzufügen.

---
+++ DistributionListUpdater/Ribbon1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DistributionListUpdater
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">"true", wenn verwaltete Ressourcen gelöscht werden sollen, andernfalls "false".</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnUpdateAllLists = this.Factory.CreateRibbonButton();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabContacts";
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabContacts";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnUpdateAllLists);
+            this.group1.Label = "Verteilerlisten";
+            this.group1.Name = "group1";
+            // 
+            // btnUpdateAllLists
+            // 
+            this.btnUpdateAllLists.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnUpdateAllLists.Image = global::DistributionListUpdater.Properties.Resources.UpdateDistLists;
+            this.btnUpdateAllLists.Label = "Alle aktualisieren";
+            this.btnUpdateAllLists.Name = "btnUpdateAllLists";
+            this.btnUpdateAllLists.ScreenTip = "Klicken Sie hier, um alle Verteilerlisten zu aktualisieren";
+            this.btnUpdateAllLists.ShowImage = true;
+            this.btnUpdateAllLists.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnUpdateAllLists_Click);
+            // 
+            // tab2
+            // 
+            this.tab2.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab2.ControlId.OfficeId = "TabMail";
+            this.tab2.Groups.Add(this.group2);
+            this.tab2.Label = "TabMail";
+            this.tab2.Name = "tab2";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button1);
+            this.group2.Label = "Verteilerlisten";
+            this.group2.Name = "group2";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = global::DistributionListUpdater.Properties.Resources.UpdateDistLists;
+            this.button1.Label = "Alle aktualisieren";
+            this.button1.Name = "button1";
+            this.button1.ScreenTip = "Klicken Sie hier, um alle Verteilerlisten zu aktualisieren";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnUpdateAllLists;
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ DistributionListUpdater/Ribbon1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+using DistributionListUpdater;
+
+namespace DistributionListUpdater
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnUpdateAllLists_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.RebuildDistributionLists();
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.RebuildDistributionLists();
+        }
+    }
+}
